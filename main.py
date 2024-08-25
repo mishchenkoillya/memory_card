1cebcd97c7d505b97b2cf75f93ba97431385eed3
Mishchenko ILLya <mishchenkoilya1505@gmail.com>
2024-08-25 13:31:39 +0300

    add menu_window ul and switch screen

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from PyQt5.QtWidgets import QApplication
 
-app= QApplication([])
+app = QApplication([])# створення додатку
 
 from main_window import *
+from menu_window import *
+
 main_window.show()
+
+def to_menu():
+    main_window.hide()
+    menu_window.show()
+
+def to_main():
+    menu_window.hide()
+    main_window.show()
+
+btn_menu.clicked.connect(to_menu)
+btn_back.clicked.connect(to_main)
 app.exec_()
 
